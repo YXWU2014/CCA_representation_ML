9d2419a67237125e42b2a47f90257f557b95796f
YXWU2014 <wyx9223@gmail.com>
2023-07-05 14:52:17 +0200

    update

--- utils/kfold_norm_preprocessing.py
+++ utils/kfold_norm_preprocessing.py
@@ -1,34 +1,59 @@
 from sklearn.model_selection import RepeatedKFold
+from matplotlib import cm
+import matplotlib.pyplot as plt
 
 
 def kfold_split_and_normalize(data_list, scalers_list, n_splits=2, n_repeats=6, random_state=None):
+    """
+    Perform k-fold cross-validation on the given data sets and apply normalization using the provided scalers.
+
+    Parameters
+    ----------
+    data_list : list of ndarray
+        List of data sets to be split. Each data set is expected to be an ndarray.
+    scalers_list : list of sklearn.preprocessing scaler
+        List of scalers corresponding to the data sets in data_list. Each scaler is used to normalize the corresponding data set.
+    n_splits : int, default=2
+        Number of folds for cross-validation.
+    n_repeats : int, default=6
+        Number of times cross-validator needs to be repeated.
+    random_state : int or RandomState instance, default=None
+        Controls the randomness of the cv splitter.
+
+    Returns
+    -------
+    Tuple of lists
+        The train/test split data for each data set in data_list and their normalized versions.
+    """
+
     cv = RepeatedKFold(n_splits=n_splits, n_repeats=n_repeats,
                        random_state=random_state)
 
     train_KFold_list, test_KFold_list = [], []
     train_norm_KFold_list, test_norm_KFold_list = [], []
 
-
-# first lets unpack the X_list and scalers_list
     for data, scaler in zip(data_list, scalers_list):
-        temp_train_KFold = []  # each will temperarily as X or Y or H
-        temp_test_KFold = []
-        temp_train_norm_KFold = []
-        temp_test_norm_KFold = []
+        # temporary lists to store split data and normalized data for each data set
+        temp_train_KFold, temp_test_KFold, temp_train_norm_KFold, temp_test_norm_KFold = [], [], [], []
 
-        # split based on the first array for all arrays in the X_list(e.g. X, Y, H)
+        # split is based on the first data set
         for _, (train_ix, test_ix) in enumerate(cv.split(data_list[0])):
 
+            # split data set
             train_array, test_array = data[train_ix], data[test_ix]
+
+            # normalize split data
             train_norm, test_norm = scaler.transform(
                 train_array), scaler.transform(test_array)
 
-            temp_train_KFold.append(train_array)  # contains the 6 CV split * 2 repeats
+            # store split data and normalized data
+            temp_train_KFold.append(train_array)
             temp_test_KFold.append(test_array)
             temp_train_norm_KFold.append(train_norm)
             temp_test_norm_KFold.append(test_norm)
 
-        train_KFold_list.append(temp_train_KFold)  # contains X, Y, H
+        # add the split and normalized data for each data set to the output lists
+        train_KFold_list.append(temp_train_KFold)
         test_KFold_list.append(temp_test_KFold)
         train_norm_KFold_list.append(temp_train_norm_KFold)
         test_norm_KFold_list.append(temp_test_norm_KFold)
@@ -36,20 +61,86 @@ def kfold_split_and_normalize(data_list, scalers_list, n_splits=2, n_repeats=6, 
     return train_KFold_list, test_KFold_list, train_norm_KFold_list, test_norm_KFold_list
 
 
-# when call the kfold_with_norm_H, one expect the return to be
-# ([X_trian_KFold, Y_train_KFold, H_train_KFold], [X_test_KFold, Y_test_KFold, H_test_KFold],
-# [X_train_norm_KFold, Y_train_norm_KFold, H_train_norm_KFold], [X_test_norm_KFold, Y_test_norm_KFold, H_test_norm_KFold])
-def kfold_with_norm_H(X, Y, H,
-                      scaler_X, scaler_features, scaler_H,
-                      n_splits, n_repeats, random_state):
+def kfold_with_norm_H(X, Y, H, scaler_X, scaler_features, scaler_H, n_splits, n_repeats, random_state):
+    """
+    Wrapper function for kfold_split_and_normalize. Applies k-fold cross-validation and normalization to the input data sets.
+
+    Returns four lists containing the split and normalized data for X, Y, and H respectively.
+    """
     return kfold_split_and_normalize([X, Y, H],
                                      [scaler_X, scaler_features, scaler_H],
                                      n_splits, n_repeats, random_state)
 
-# when call the kfold_with_norm_C, one expect the return to be
-# ([X_trian_KFold, Z_train_KFold, W_train_KFold, C_train_KFold], [X_test_KFold, Z_test_KFold, W_test_KFold, C_test_KFold],
-# [X_train_norm_KFold, Z_train_norm_KFold, W_train_norm_KFold, C_train_norm_KFold], [X_test_norm_KFold, Z_test_norm_KFold, W_test_norm_KFold, C_test_norm_KFold])
-
 
 def kfold_with_norm_C(X, Z, W, C, scaler_X, scaler_testing, scaler_features, scaler_C, n_splits, n_repeats, random_state):
-    return kfold_split_and_normalize([X, Z, W, C], [scaler_X, scaler_testing, scaler_features, scaler_C], n_splits, n_repeats, random_state)
+    """
+    Wrapper function for kfold_split_and_normalize. Applies k-fold cross-validation and normalization to the input data sets.
+
+    Returns four lists containing the split and normalized data for X, Z, W, and C respectively.
+    """
+    return kfold_split_and_normalize([X, Z, W, C],
+                                     [scaler_X, scaler_testing,
+                                         scaler_features, scaler_C],
+                                     n_splits, n_repeats, random_state)
+
+
+def plot_hist_kfold_with_norm(train_data, test_data, x_min, x_max, axs_title, n_splits, n_repeats, nrows=3, dataset='H'):
+    """
+    This function plots histograms for each fold of cross-validation in the K-Fold normalization scheme.
+
+    Parameters:
+    train_data (list of np.array): List of training data arrays for each feature.
+    test_data (list of np.array): List of test data arrays for each feature.
+    x_min (list): List of minimum x-values for the plots corresponding to each feature.
+    x_max (list): List of maximum x-values for the plots corresponding to each feature.
+    axs_title (str): Title for the plot.
+    n_splits (int): Number of folds in the K-Fold split.
+    n_repeats (int): Number of repetitions in the K-Fold split.
+    nrows (int, optional): Number of rows in the subplot grid. Default is 3.
+    dataset (str, optional): Identifier for the type of dataset ('H' or 'C'). Default is 'H'.
+
+    Returns:
+    None
+    """
+
+    # Create subplot grid
+    fig, axs = plt.subplots(nrows=nrows, ncols=n_splits *
+                            n_repeats, figsize=(30, 4*nrows))
+
+    # Define color maps
+    colors_1, colors_2 = cm.get_cmap('Blues', 10), cm.get_cmap('Reds', 10)
+
+    # Select the data to be plotted based on the dataset type
+    if dataset == 'H':
+        dataset_plot = [(train_data[0], test_data[0], x_min[0], x_max[0], 'compo'),
+                        (train_data[1], test_data[1],
+                         x_min[1], x_max[1], 'H/C specific'),
+                        (train_data[2], test_data[2], x_min[2], x_max[2], 'output')]
+    elif dataset == 'C':
+        dataset_plot = [(train_data[0], test_data[0], x_min[0], x_max[0], 'compo'),
+                        (train_data[1], test_data[1],
+                         x_min[1], x_max[1], 'C testing'),
+                        (train_data[2], test_data[2],
+                         x_min[2], x_max[2], 'H/C specific'),
+                        (train_data[3], test_data[3], x_min[3], x_max[3], 'output')]
+
+    # Loop over each split and repeat in the K-Fold scheme
+    for i in range(n_splits*n_repeats):
+        # Loop over each feature to be plotted
+        for j, data in enumerate(dataset_plot):
+            # Unpack data
+            train_data_j, test_data_j, x_min_j, x_max_j, axs_title_j = data
+            # Plot histogram for the training data
+            axs[j, i].hist(train_data_j[i], bins=10, edgecolor='black',
+                           color=colors_1(range(len(train_data_j[i][0]))))
+            # Plot histogram for the test data
+            axs[j, i].hist(test_data_j[i],  bins=10, edgecolor='black',
+                           color=colors_2(range(len(test_data_j[i][0]))))
+            # Set title and x-axis limits for the plot
+            axs[j, i].set_title(f'Fold {i+1}_' + axs_title_j)
+            axs[j, i].set_xlim([x_min_j, x_max_j])
+
+    # Set main title and adjust layout for the entire figure
+    fig.suptitle(axs_title, fontsize=24)
+    fig.tight_layout()
+    plt.show()
