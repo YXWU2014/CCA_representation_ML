08b2f2c4db035a110c35f985227e271fb75869f9
YXWU2014 <wyx9223@gmail.com>
2023-07-14 11:32:29 +0200

    update

---
+++ utils/postprocessing_evalutation.py
@@ -0,0 +1,265 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
+from tabulate import tabulate
+from joblib import Parallel, delayed
+import tensorflow as tf
+from tensorflow import keras
+
+
+def display_saved_models(model_path_bo):
+    """
+    This function displays the saved NNH and NNC models in a tabular format.
+
+    Parameters:
+    model_path_bo: str
+        The path of the directory where the model files are stored.
+
+    The function sorts the models based on their type (NNH or NNC) and their index 
+    (as mentioned in the filename after 'RepeatedKFold_'). It then displays them in a table.
+    """
+
+    # Get all h5 files from the specified directory
+    files = sorted([f for f in os.listdir(model_path_bo) if f.endswith('.h5')])
+
+    # Separate NNH and NNC model files
+    nnh_files = [f for f in files if f.startswith('NNH_model_RepeatedKFold')]
+    nnc_files = [f for f in files if f.startswith('NNC_model_RepeatedKFold')]
+
+    # Sort model files based on the index present in the filename
+    nnh_files.sort(key=lambda x: int(x.split("_")[-1].split(".")[0]))
+    nnc_files.sort(key=lambda x: int(x.split("_")[-1].split(".")[0]))
+
+    # Prepare the table data with model filenames
+    table_data = [["NNH_model", "NNC_model"]]
+    for i in range(12):
+        nnh_file = nnh_files[i] if i < len(nnh_files) else ""
+        nnc_file = nnc_files[i] if i < len(nnc_files) else ""
+        table_data.append([nnh_file, nnc_file])
+
+    # Display the model filenames in tabular format
+    print(tabulate(table_data, headers="firstrow"))
+
+
+def predict_bootstrap(model_path, model_name,
+                      X1_list, Y1_list, V1_list,
+                      k_folds, n_CVrepeats, mc_repeat,
+                      scaler_compo, scaler_testing, scaler_specific, scaler_output):
+    """
+    Perform bootstrap predictions on a given model with specified parameters.
+
+    Parameters:
+    model_path: str
+        The path of the directory where the model file is stored.
+    model_name: str
+        The filename of the model.
+    X1_list, Y1_list, V1_list: list of np.array
+        List of test data arrays.
+    k_folds: int
+        The number of folds for k-fold cross-validation.
+    n_CVrepeats: int
+        The number of repeats for k-fold cross-validation.
+    mc_repeat: int
+        The number of Monte Carlo simulations to perform.
+    scaler_compo, scaler_testing, scaler_specific, scaler_output: sklearn.preprocessing Scalers
+        Scalers for normalizing input data and inverse transforming output data.
+
+    Returns:
+    H1_pred_X1_list, H1_pred_X1_mc_mean, H1_pred_X1_mc_std: lists
+        Lists of predictions, means, and standard deviations of predictions.
+    """
+    # Initialize lists for storing prediction results
+    H1_pred_X1_list = []
+    H1_pred_X1_mc_mean = []
+    H1_pred_X1_mc_std = []
+
+    # Function to predict on one fold of the cross-validated model
+    def predict_one_model(i):
+
+        # Load the ith model
+        NNH_model_loaded_temp = keras.models.load_model(
+            os.path.join(model_path, model_name.format(i+1)))
+
+        # Normalize input data using the provided scalers
+        X1_temp_norm = scaler_compo.transform(X1_list[i])
+        V1_temp_norm = scaler_specific.transform(V1_list[i])
+
+        # Concatenate normalized X1 and V1 data
+        X1_V1_temp_norm = np.concatenate([X1_temp_norm, V1_temp_norm], axis=1)
+
+        # Check if testing condition for C is defined
+        if len(Y1_list) != 0:
+            Y1_temp_norm = scaler_testing.transform(Y1_list[i])
+
+            def predict_one_sample(): return scaler_output.inverse_transform(
+                NNH_model_loaded_temp.predict(
+                    [X1_V1_temp_norm, Y1_temp_norm], verbose=0)
+            )
+        else:  # if testing condition for H is NOT defined
+            def predict_one_sample(): return scaler_output.inverse_transform(
+                NNH_model_loaded_temp.predict(X1_V1_temp_norm, verbose=0)
+            )
+
+        # Perform Monte Carlo simulations for predictions
+        H1_pred_X1_mc_stack_temp = tf.map_fn(lambda _: predict_one_sample(),
+                                             tf.range(mc_repeat),
+                                             dtype=tf.float32,
+                                             parallel_iterations=mc_repeat)
+
+        # Compute mean and standard deviation of the predictions
+        H1_pred_X1_mc_mean_temp = np.mean(
+            H1_pred_X1_mc_stack_temp, axis=0).reshape((-1,))
+        H1_pred_X1_mc_std_temp = np.std(
+            H1_pred_X1_mc_stack_temp,  axis=0).reshape((-1,))
+
+        return H1_pred_X1_mc_stack_temp, H1_pred_X1_mc_mean_temp, H1_pred_X1_mc_std_temp
+
+    # Perform parallel prediction on each fold of the cross-validated model
+    results = Parallel(n_jobs=-1)(delayed(predict_one_model)(i)
+                                  for i in range(k_folds * n_CVrepeats))
+
+    # Clear TensorFlow session to free up resources
+    tf.keras.backend.clear_session()
+
+    # Extract and return results
+    for mc_stack, mean, std in results:
+        H1_pred_X1_list.append(mc_stack)
+        H1_pred_X1_mc_mean.append(mean)
+        H1_pred_X1_mc_std.append(std)
+
+    return H1_pred_X1_list, H1_pred_X1_mc_mean, H1_pred_X1_mc_std
+
+
+def plot_test_true_vs_pred(k_folds, n_CVrepeats, test_KFold, test_pred_mean, test_pred_std, lims, label, color, model_path_bo):
+    """
+    Plot true vs predicted values for test data across multiple cross-validation folds.
+
+    Parameters:
+    k_folds: int
+        The number of folds for k-fold cross-validation.
+    n_CVrepeats: int
+        The number of repeats for k-fold cross-validation.
+    test_KFold: list of np.array
+        List of ground truth values for each fold.
+    test_pred_mean: list of np.array
+        List of mean prediction values for each fold.
+    test_pred_std: list of np.array
+        List of standard deviation of prediction values for each fold.
+    lims: tuple
+        Tuple specifying the limits of the plot (xmin, xmax, ymin, ymax).
+    label: str
+        Label for the data being plotted.
+    color: str
+        Color for the data points.
+    model_path_bo: str
+        The path to the directory where the plot will be saved.
+
+    Returns:
+    None
+    """
+    # Initialize a new matplotlib figure
+    fig, ax = plt.subplots(nrows=2, ncols=6, figsize=(18, 7))
+
+    # Iterate over each fold
+    for i in range(k_folds * n_CVrepeats):
+        # Determine the row and column index for the subplot
+        row_idx = i // 6
+        col_idx = i % 6
+
+        # Set the limits of the subplot and draw the line y=x
+        ax[row_idx, col_idx].set_xlim(lims)
+        ax[row_idx, col_idx].set_ylim(lims)
+        ax[row_idx, col_idx].plot(lims, lims, color='grey')
+
+        # Scatter plot of true vs predicted values
+        ax[row_idx, col_idx].scatter(
+            test_KFold[i], test_pred_mean[i], label=label, color=color, alpha=0.5)
+
+        # Add error bars to the scatter plot
+        ax[row_idx, col_idx].errorbar(x=test_KFold[i], y=test_pred_mean[i], yerr=test_pred_std[i], fmt='none',
+                                      ecolor=color, capsize=3, alpha=0.5)
+
+        # Compute and add R^2 score to the subplot
+        r = r2_score(test_KFold[i], test_pred_mean[i])
+        ax[row_idx, col_idx].text(.05, .7, 'r2={:.2f}'.format(
+            r), transform=ax[row_idx, col_idx].transAxes, color=color)
+
+        # Set labels and aspect ratio for the subplot
+        ax[row_idx, col_idx].set_xlabel('True values in training dataset')
+        ax[row_idx, col_idx].set_ylabel('Predictions')
+        ax[row_idx, col_idx].set_aspect('equal', 'box')
+
+        # Add a legend to the subplot
+        ax[row_idx, col_idx].legend(loc=4, prop={'size': 8})
+
+        # Enable grid for the subplot
+        ax[row_idx, col_idx].grid()
+
+    # Adjust spacing and add title
+    fig.tight_layout()
+    axs_title = label + '_RepeatedKFold_True_Prediction_testdata'
+    fig.suptitle(axs_title, fontsize=18)
+
+    # Save the figure and display the plot
+    plt.savefig(os.path.join(model_path_bo, axs_title + '.png'),
+                bbox_inches='tight')
+    plt.show()
+
+
+def plot_full_true_vs_pred(HC_list, HC_pred_stack_list, model_path_bo, lims):
+    """
+    Plot true vs predicted values for a model's full output.
+
+    Parameters:
+    HC_list: list
+        A list of numpy arrays representing the true outputs of the models.
+    HC_pred_stack_list: list
+        A list of numpy arrays representing the predicted outputs of the models.
+    model_path_bo: str
+        The path to the directory where the plot will be saved.
+    lims: tuple
+        Tuple specifying the limits of the plot (xmin, xmax, ymin, ymax).
+
+    Returns:
+    None
+    """
+
+    # Compute mean and standard deviation of the first model's predictions
+    H1_pred_X1_conc = np.concatenate(HC_pred_stack_list[0], axis=0)
+    H1_pred_X1_KFold_mean = np.mean(H1_pred_X1_conc, axis=0).reshape(-1)
+    H1_pred_X1_KFold_std = np.std(H1_pred_X1_conc, axis=0).reshape(-1)
+
+    # Compute mean and standard deviation of the second model's predictions
+    C2_pred_X2_conc = np.concatenate(HC_pred_stack_list[1], axis=0)
+    C2_pred_X2_KFold_mean = np.mean(C2_pred_X2_conc, axis=0).reshape(-1)
+    C2_pred_X2_KFold_std = np.std(C2_pred_X2_conc, axis=0).reshape(-1)
+
+    # Initialize a new matplotlib figure
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
+
+    # Data, labels and colors for each subplot
+    data_labels_colors = [
+        ((HC_list[0], H1_pred_X1_KFold_mean, H1_pred_X1_KFold_std),
+         'NNH_model', 'steelblue'),
+        ((HC_list[1], C2_pred_X2_KFold_mean,
+         C2_pred_X2_KFold_std), 'NNC_model', 'firebrick')
+    ]
+
+    # Create each subplot
+    for i, (data, label, color) in enumerate(data_labels_colors):
+        ax[i].set(xlim=lims[i], ylim=lims[i], aspect='equal', box_aspect=1,
+                  xlabel='True values in training dataset', ylabel='Predictions',
+                  title=f'{label} - r2={r2_score(data[0], data[1]):.2f}')
+        ax[i].plot(lims[i], lims[i], color='grey')
+        ax[i].scatter(*data[:2], label=label, color=color, alpha=0.5)
+        ax[i].errorbar(x=data[0], y=data[1], yerr=data[2],
+                       fmt='none', ecolor=color, capsize=3, alpha=0.3)
+        ax[i].legend(loc=4, prop={'size': 8})
+        ax[i].grid()
+
+    # Adjust spacing and save the figure
+    fig.tight_layout()
+    plt.savefig(os.path.join(
+        model_path_bo, 'NN_full_RepeatedKFold_True_Prediction_fulldata.png'), bbox_inches='tight')
+    plt.show()
---
+++ utils/preprocessing_kfold_norm.py
@@ -0,0 +1,150 @@
+from sklearn.model_selection import RepeatedKFold
+from matplotlib import cm
+import matplotlib.pyplot as plt
+
+
+def kfold_split_and_normalize(data_list, scalers_list, n_splits=2, n_repeats=6, random_state=None):
+    """
+    Perform k-fold cross-validation on the given data sets and apply normalization using the provided scalers.
+
+    Parameters
+    ----------
+    data_list : list of ndarray
+        List of data sets to be split. Each data set is expected to be an ndarray.
+    scalers_list : list of sklearn.preprocessing scaler
+        List of scalers corresponding to the data sets in data_list. Each scaler is used to normalize the corresponding data set.
+    n_splits : int, default=2
+        Number of folds for cross-validation.
+    n_repeats : int, default=6
+        Number of times cross-validator needs to be repeated.
+    random_state : int or RandomState instance, default=None
+        Controls the randomness of the cv splitter.
+
+    Returns
+    -------
+    Tuple of lists
+        The train/test split data for each data set in data_list and their normalized versions.
+    """
+
+    cv = RepeatedKFold(n_splits=n_splits, n_repeats=n_repeats,
+                       random_state=random_state)
+
+    train_KFold_list, test_KFold_list = [], []
+    train_norm_KFold_list, test_norm_KFold_list = [], []
+
+    for data, scaler in zip(data_list, scalers_list):
+        # temporary lists to store split data and normalized data for each data set
+        temp_train_KFold, temp_test_KFold, temp_train_norm_KFold, temp_test_norm_KFold = [], [], [], []
+
+        # split is based on the first data set
+        for _, (train_ix, test_ix) in enumerate(cv.split(data_list[0])):
+
+            # split data set
+            train_array, test_array = data[train_ix], data[test_ix]
+
+            # normalize split data
+            train_norm, test_norm = scaler.transform(
+                train_array), scaler.transform(test_array)
+
+            # store split data and normalized data
+            temp_train_KFold.append(train_array)
+            temp_test_KFold.append(test_array)
+            temp_train_norm_KFold.append(train_norm)
+            temp_test_norm_KFold.append(test_norm)
+
+        # add the split and normalized data for each data set to the output lists
+        train_KFold_list.append(temp_train_KFold)
+        test_KFold_list.append(temp_test_KFold)
+        train_norm_KFold_list.append(temp_train_norm_KFold)
+        test_norm_KFold_list.append(temp_test_norm_KFold)
+
+    return train_KFold_list, test_KFold_list, train_norm_KFold_list, test_norm_KFold_list
+
+
+def kfold_with_norm_H(X, V, H,
+                      scaler_compo, scaler_features, scaler_output,
+                      n_splits, n_repeats, random_state):
+    """
+    Wrapper function for kfold_split_and_normalize. Applies k-fold cross-validation and normalization to the input data sets.
+
+    Returns four lists containing the split and normalized data for X, V, and H respectively.
+    """
+    return kfold_split_and_normalize([X, V, H],
+                                     [scaler_compo, scaler_features, scaler_output],
+                                     n_splits, n_repeats, random_state)
+
+
+def kfold_with_norm_C(X, Z, W, C,
+                      scaler_compo, scaler_testing, scaler_features, scaler_output,
+                      n_splits, n_repeats, random_state):
+    """
+    Wrapper function for kfold_split_and_normalize. Applies k-fold cross-validation and normalization to the input data sets.
+
+    Returns four lists containing the split and normalized data for X, Z, W, and C respectively.
+    """
+    return kfold_split_and_normalize([X, Z, W, C],
+                                     [scaler_compo, scaler_testing,
+                                         scaler_features, scaler_output],
+                                     n_splits, n_repeats, random_state)
+
+
+def plot_hist_kfold_with_norm(train_data, test_data, x_min, x_max, axs_title, n_splits, n_repeats, nrows=3, dataset='H'):
+    """
+    This function plots histograms for each fold of cross-validation in the K-Fold normalization scheme.
+
+    Parameters:
+    train_data (list of np.array): List of training data arrays for each feature.
+    test_data (list of np.array): List of test data arrays for each feature.
+    x_min (list): List of minimum x-values for the plots corresponding to each feature.
+    x_max (list): List of maximum x-values for the plots corresponding to each feature.
+    axs_title (str): Title for the plot.
+    n_splits (int): Number of folds in the K-Fold split.
+    n_repeats (int): Number of repetitions in the K-Fold split.
+    nrows (int, optional): Number of rows in the subplot grid. Default is 3.
+    dataset (str, optional): Identifier for the type of dataset ('H' or 'C'). Default is 'H'.
+
+    Returns:
+    None
+    """
+
+    # Create subplot grid
+    fig, axs = plt.subplots(nrows=nrows, ncols=n_splits *
+                            n_repeats, figsize=(30, 4*nrows))
+
+    # Define color maps
+    colors_1, colors_2 = cm.get_cmap('Blues', 10), cm.get_cmap('Reds', 10)
+
+    # Select the data to be plotted based on the dataset type
+    if dataset == 'H':
+        dataset_plot = [(train_data[0], test_data[0], x_min[0], x_max[0], 'compo'),
+                        (train_data[1], test_data[1],
+                         x_min[1], x_max[1], 'H/C specific'),
+                        (train_data[2], test_data[2], x_min[2], x_max[2], 'output')]
+    elif dataset == 'C':
+        dataset_plot = [(train_data[0], test_data[0], x_min[0], x_max[0], 'compo'),
+                        (train_data[1], test_data[1],
+                         x_min[1], x_max[1], 'C testing'),
+                        (train_data[2], test_data[2],
+                         x_min[2], x_max[2], 'H/C specific'),
+                        (train_data[3], test_data[3], x_min[3], x_max[3], 'output')]
+
+    # Loop over each split and repeat in the K-Fold scheme
+    for i in range(n_splits*n_repeats):
+        # Loop over each feature to be plotted
+        for j, data in enumerate(dataset_plot):
+            # Unpack data
+            train_data_j, test_data_j, x_min_j, x_max_j, axs_title_j = data
+            # Plot histogram for the training data
+            axs[j, i].hist(train_data_j[i], bins=10, edgecolor='black',
+                           color=colors_1(range(len(train_data_j[i][0]))))
+            # Plot histogram for the test data
+            axs[j, i].hist(test_data_j[i],  bins=10, edgecolor='black',
+                           color=colors_2(range(len(test_data_j[i][0]))))
+            # Set title and x-axis limits for the plot
+            axs[j, i].set_title(f'Fold {i+1}_' + axs_title_j)
+            axs[j, i].set_xlim([x_min_j, x_max_j])
+
+    # Set main title and adjust layout for the entire figure
+    fig.suptitle(axs_title, fontsize=24)
+    fig.tight_layout()
+    plt.show()
